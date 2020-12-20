b35c433fb516ae7b83eaacd7ff90353a2ec4e391
thanhnc <thanhnc138@mail.com>
2020-12-20 16:22:25 +0700

    update os path

--- server/app.py
+++ server/app.py
@@ -7,6 +7,7 @@ import os
 import io
 import base64
 from PIL import Image
+import image_reader
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -22,21 +23,6 @@ def get_example():
     response = jsonify(message="Simple server is running")
     return response
 
-def load_images_from_folder(folder):
-    images = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder, filename))
-        if img is not None:
-            images.append(img)
-    return images
-
-def get_encoded_img(image_path):
-    img = Image.open(image_path, mode='r')
-    img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    my_encoded_img = base64.encodebytes(img_byte_arr.getvalue()).decode('ascii')
-    return my_encoded_img
-
 @app.route("/upload", methods=["POST"])
 @cross_origin()
 def upload():
@@ -47,35 +33,30 @@ def upload():
 
         # save raw image
         path = os.getcwd()
-        print(path)
         raw_image_directory = os.path.join(path, 'raw_image')
-        print(raw_image_directory)
         if not os.path.exists(raw_image_directory):
             os.makedirs(raw_image_directory)
         f = request.files.get('file')
         raw_path = os.path.join(raw_image_directory, file_name)
-        print(raw_path)
         f.save(raw_path)
 
         # process image
 
         # save result
         final_result_directory = os.path.join(path, 'final_result')
-        print(final_result_directory)
         if not os.path.exists(final_result_directory):
             os.makedirs(final_result_directory)
         result_path = os.path.join(final_result_directory, file_name)
         f.save(result_path)
-        result_image = get_encoded_img(raw_path)
+        result_image = image_reader.get_encoded_img(result_path)
         return jsonify({
             "result_image": "data:image/" + file_extension + ";base64," + result_image
-        })
+        }), 200
     except Exception as e:
-        print('Exception: ')
-        print(e)
+        print('Exception: ' + str(e))
         return jsonify({
             'error': str(e)
-        })
+        }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
